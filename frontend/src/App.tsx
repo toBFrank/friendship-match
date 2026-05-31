6dd275cf310e5f9c451a23eefe81c50b7b303933
Franco Bonilla <bonilla.franco484@gmail.com>
2026-05-31 15:02:35 -0600

    deleted old frontend files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,19 +1,8 @@
 import { Route, Routes } from "react-router";
-import Landing from "./pages/Landing/Landing";
-import Matching from "./pages/Matching/Matching";
-import Authentication from "./pages/Authentication/Authentication";
 
 function App() {
   return (
     <Routes>
-      <Route path="" element={<Landing />} />
-      <Route path="matching" element={<Matching />} />
-      {/* <Route path="results" element={<Results />} /> */}
-      <Route path="auth" element={<Authentication />} />
-
-      {/* <Route path="login" element={<Login />} /> */}
-      {/* <Route path="profile" element={<Profile />} /> */}
-      {/* <Route path="chat" element={<Chat />} /> */}
     </Routes>
   );
 }
