500e09fa923d1a8f1ddecf88712097c4aa8e0dc0
Franco Bonilla <bonilla.franco484@gmail.com>
2026-06-01 18:51:30 -0600

    Added a base page

---
+++ frontend/src/pages/BasePage.tsx
@@ -0,0 +1,20 @@
+import { Header, Footer } from "../components/layout";
+
+export default function BasePage() {
+  return (
+    <>
+      <a href="#main-content">Skip to main content</a>
+
+      <Header />
+
+      <main id="main-content">
+        <section aria-labelledby="section-heading">
+          <h1 id="section-heading">Page Title</h1>
+          <p>Content here.</p>
+        </section>
+      </main>
+
+      <Footer />
+    </>
+  );
+}
