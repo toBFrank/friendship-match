5764c5f2b9500bab7ef7eca49906f83f2c89625b
Franco Bonilla <bonilla.franco484@gmail.com>
2026-06-02 15:24:22 -0600

    Added tailwind css framework. Added Geist Mono font (to be replaced when finalizing app).

--- frontend/src/components/layout/Header.tsx
+++ frontend/src/components/layout/Header.tsx
@@ -1,20 +1,18 @@
 export default function Header() {
   // TODO: Finalize design and styling of the header
   // TODO: Finalize app name and branding to include in the header
+  const APP_NAME = "Flocker";
+  const NAV_LINKS = [{ name: "Questionnaire", href: "/questionnaire" }];
   return (
-    <header>
+    <header className="w-full border-b border-black px-6 py-4">
       <nav aria-label="Main navigation">
-        <a href="/">Flocker</a>
+        <a href="/">{APP_NAME}</a>
         <ul>
-          <li>
-            <a href="/questionnaire">Questionnaire</a>
-          </li>
-          <li>
-            <a href="/matches">Matches</a>
-          </li>
-          <li>
-            <a href="/profile">Profile</a>
-          </li>
+          {NAV_LINKS.map((link) => (
+            <li key={link.name}>
+              <a href={link.href}>{link.name}</a>
+            </li>
+          ))}
         </ul>
       </nav>
     </header>
